95b915413ad393952e791c8ee1c7087c0f733d46
jobucaldas <joaovictorbuenodecaldas@gmail.com>
2019-07-12 16:42:07 -0300

    Updated main

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace sf;
 
-void draw_menu(int* current_scr, RenderWindow* window) {
+void draw_menu(int* current_scr, RenderWindow &window) {
 	// Create font
 	Font font;
 	if (!font.loadFromFile("fonts/pc98.ttf")) {
@@ -11,21 +11,31 @@ void draw_menu(int* current_scr, RenderWindow* window) {
 		exit(1);
 	}
 	Text logo;
+	logo.setFont(font);
 	logo.setString("Afraid Of The Shadows");
 	logo.setCharacterSize(24);
-	logo.setPosition(1024 / 2 - (24 * 21) / 2 / 2, 576 / 2 - 24 / 2);
+	logo.setPosition(1024 / 2 - (24 * 21) / 2 / 2, 576 / 2 / 2 - 24 / 2);
 
-	cout << "Here" << endl;
-
-	window->clear();
-	window->draw(logo);
+	window.clear();
+	window.draw(logo);
 }
 
-void draw_scr(int* current_scr, RenderWindow* window) {
-	cout << "Current scr = " << *current_scr << endl;
+void draw_game(int* current_scr, RenderWindow& window) {
+
+}
+
+void draw_scr(int* current_scr, RenderWindow &window) {
+	/*
+	 *  0 - Menu
+	 *  1 - Game
+	 */
+
 	switch (*current_scr) {
 	case 0:
 		draw_menu(current_scr, window);
+		break;
+	case 1:
+		draw_game(current_scr, window);
 		break;
 	default:
 		break;
@@ -36,7 +46,7 @@ int main(void) {
 	int current_scr[1] = { 0 };
 
 	// Initialize window
-	RenderWindow window(VideoMode(1024, 576), "Afraid Of The Shadows", Style::Resize | Style::Close | Style::Titlebar);
+	RenderWindow window(VideoMode(1024, 576), "Afraid Of The Shadows", Style::Resize | Style::Close);
 	window.setFramerateLimit(60);
 
 	while (window.isOpen()) {
@@ -47,7 +57,7 @@ int main(void) {
 				window.close();
 		}
 
-		draw_scr(current_scr, &window);
+		draw_scr(current_scr, window);
 		window.display();
 	}
 
